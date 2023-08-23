56dcc418e101080ec7c8eb2afb4f1cc2a9882caf
Kay <burteendavi@gmail.com>
2023-08-23 09:55:57 +0300

    Added prac3

---
+++ fibonacci.py
@@ -0,0 +1,7 @@
+a, b = 0, 1
+print("First 10 numbers of the Fibonacci sequence: ")
+
+for _ in range (10):
+    print (a)
+
+    a, b = b, a + b
---
+++ repeated_user_input.py
@@ -0,0 +1,4 @@
+while True:
+    user_input = input("Enter something (type 'quit' or 'exit' to stop): ").lower()
+    if user_input == "quit" or user_input == "exit":
+        break
