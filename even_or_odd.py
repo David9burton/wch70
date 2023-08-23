5f749a49f9bbfcf983309b74d7b04c41ffc6cdfb
Kay <burteendavi@gmail.com>
2023-08-23 12:28:38 +0300

    Improved the code

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,7 +1,12 @@
-number = int(input("Enter a number: "))
-
-if number % 2 == 0:
-    print(f"{number} is even.")
-else:
-    print(f"{number} is odd.")
+while True:
+    try:
+        number = int(input("Enter a number (or type 'exit' to quite): "))
+        if number % 2 == 0:
+            print(f"{number} is even.")
+        else:
+            print(f"{number} is odd.")
+    except ValueError:
+        print("Please enter a valid number or type 'exit' to quit.")
+    if input("Do you want to check another number? (yes/no): ").lower() != 'yes':
+        break
     
