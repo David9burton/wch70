c7892e6966efe82e9e2a89504d13d3360c23c442
Kay <burteendavi@gmail.com>
2023-08-24 21:42:02 +0300

    Day4 haha

---
+++ add.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = add(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
---
+++ mathoperations.py
@@ -0,0 +1,7 @@
+import math_operations
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = math_operations.add(num1, num2)
+print(f"The sum of {num1} and {num2} is: {result}")
